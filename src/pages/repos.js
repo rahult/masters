b5188e9d13fabb73a1de3307b1acfcde64bfc616
Rahul Trikha <rahul.trikha@gmail.com>
2015-06-27 21:12:30 +0930

    Fixed eslint related warnings

---
+++ src/pages/repos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass({
+
+  render () {
+    return (
+      <div>
+        <h2>Repos</h2>        
+      </div>
+    )
+  }
+
+})
---
+++ src/router.js
@@ -0,0 +1,33 @@
+import Router from 'ampersand-router'
+import React from 'react'
+import HomePage from './pages/home'
+import ReposPage from './pages/repos'
+import LayoutPage from './layout'
+
+
+export default Router.extend({
+  renderPage (page, opts = {layout: true}) {
+    if(opts.layout) {
+      page = (
+        <LayoutPage>
+          {page}
+        </LayoutPage>
+      )
+    }
+
+    return React.render(page, document.body)
+  },
+
+  routes: {
+    '': 'home',
+    'repos': 'repos'
+  },
+
+  home () {
+    this.renderPage(<HomePage/>, {layout: false})
+  },
+
+  repos () {
+    this.renderPage(<ReposPage/>)
+  }
+})
